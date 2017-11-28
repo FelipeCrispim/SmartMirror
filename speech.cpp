d188b2921783380cd13d6ab3d4be765fa49174ad
FelipeCrispim <fcc@ic.ufal.br>
2017-11-28 11:37:32 -0300

    Adding audio

--- speech.cpp
+++ speech.cpp
@@ -40,37 +40,23 @@ void Speech::say(int hour, QString weather)
     }
 
     // https://www.wunderground.com/weather/api/d/docs?d=resources/phrase-glossary&MR=1&_ga=2.183219110.1875570343.1511699835-1348404951.1511699835
-    if(hour < 17) {
-        if(weather == "mostlycloudy" || weather == "mostlysunny" ||
-                weather == "partlycloudy" || weather == "partlysunny") {
-            message += "Parece que hoje, será um dia com algumas nuvens.";
-        } else if(weather == "sunny" || weather == "clear") {
-            message += "Parece que hoje, será um ótimo dia ensolarado.";
-        } else {
-            message += "A expectativa para hoje, é de risco de chuva.";
-        }
-    }
+//    if(hour < 17) {
+//        if(weather == "mostlycloudy" || weather == "mostlysunny" ||
+//                weather == "partlycloudy" || weather == "partlysunny") {
+//            message += "Parece que hoje, será um dia com algumas nuvens.";
+//        } else if(weather == "sunny" || weather == "clear") {
+//            message += "Parece que hoje, será um ótimo dia ensolarado.";
+//        } else {
+//            message += "A expectativa para hoje, é de risco de chuva.";
+//        }
+//    }
 
-    #if __APPLE__
-//        m_speech->say(message);
-    #else
-//    QString program = "/home/felipecrispim/Qt-Workspace/smartmirror2";
-//    QStringList arguments, arg2;
-//    arguments << program;
-
-//    QProcess *myProcess = new QProcess(this);
-//    myProcess->start("cd", arguments);
-//    myProcess->waitForReadyRead();
-//    arg2 << "speech.sh" << message;
-//    myProcess->start("./", arg2);
-//    myProcess->waitForReadyRead();
-#endif
 
 //        QString tempMsg = "./media/smartmirror2/speech.sh "+message;
-//        QString tempMsg = "omxplayer -o hdmi http://translate.google.com/translate_tts?ie=UTF-8&client=tw-ob&q=Alo%20Alo&tl=Pt-br";
+        QString tempMsg = "omxplayer -o hdmi http://translate.google.com/translate_tts?ie=UTF-8&client=tw-ob&q="+message+"&tl=Pt-br";
 //        QProcess process;
-        QString messages = "omxplayer -o hdmi "+pathToAudioProject+"/welcome.mp3";
-        m_process->start(messages);
+//        QString messages = "omxplayer -o hdmi "+pathToAudioProject+"/weather.mp3";
+        m_process->start(tempMsg);
 
 }
 
@@ -92,10 +78,12 @@ void Speech::sayGoodBye()
 
 void Speech::infoAboutWeather(int wind, float levelSea, QString time)
 {
-    QString message = "\"Nesse momento em Maceió, a velocidade do vento é de "+QString::number(wind)+" km/h,"
-                        " e o nível da maré, é de "+QString::number(levelSea)+"m até às "+time+"\"";
+//    QString message = "\"Nesse momento em Maceió, a velocidade do vento é de "+QString::number(wind)+" km/h,"
+//                        " e o nível da maré, é de "+QString::number(levelSea)+"m até às "+time+"\"";
 //    QProcess process;
-    QString tempMsg = "omxplayer -o hdmi http://translate.google.com/translate_tts?ie=UTF-8&client=tw-ob&q="+message+"&tl=Pt-br";
-    system(tempMsg.toLatin1());
+    QString message = "omxplayer -o hdmi "+pathToAudioProject+"/weather.mp3";
+    QProcess process;
+    process.start(message);
+//    system(message);
 
 }
