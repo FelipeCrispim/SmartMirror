5a2af1e5a14187b071c3dfe02795882ec3919159
FelipeCrispim <fcc@ic.ufal.br>
2017-11-29 11:23:07 -0300

    Fixed minor bugs

--- translate.py
+++ translate.py
@@ -2,4 +2,4 @@ import urllib
 import subprocess
 import sys
 
-subprocess.call(["mpg123","http://translate.google.com/translate_tts?ie=UTF-8&client=tw-ob&q="+sys.argv[1]+"&tl=Pt-br"], shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+subprocess.call(["omxplayer","http://translate.google.com/translate_tts?ie=UTF-8&client=tw-ob&q="+sys.argv[1]+"&tl=Pt-br"], shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
